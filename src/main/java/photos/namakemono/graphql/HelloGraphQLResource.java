96701f6ea5038878436e581f4ba3fa59d737e202
kaoru.watanabe <kaoru.watanabe@shinesoft.co.jp>
2023-03-14 23:32:22 +0900

    support jdtls debug

--- src/main/java/photos/namakemono/graphql/HelloGraphQLResource.java
+++ src/main/java/photos/namakemono/graphql/HelloGraphQLResource.java
@@ -1,16 +1,22 @@
 package photos.namakemono.graphql;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.microprofile.graphql.DefaultValue;
-import org.eclipse.microprofile.graphql.Description;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Query;
+
 
 @GraphQLApi
 public class HelloGraphQLResource {
 
     @Query
-    @Description("Say hello")
-    public String sayHello(@DefaultValue("World") String name) {
+    public String hello(@DefaultValue("World") String name) {
+        List<String> list = new ArrayList<String>();
+        list.add("hoge");
+        System.out.println("hoge");
         return "Hello " + name;
     }
+
 }
